b45cf6b76714034414eba73f39c4024aa2870881
Vishal Prajapati <kumaraditya79301@gmail.com>
2025-05-09 18:32:56 +0530

    database connect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com", "rzccjqvjetzvahlwjsdj.supabase.co"],
+  },
+};
 
 export default nextConfig;
